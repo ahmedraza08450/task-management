0981fd2491fc92a7a7fc30f764d71bb2cfa48590
ahmedraza08450 <ahmedgoofy6@gmail.com>
2026-05-18 20:52:32 +0500

    Error Handling

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ import cookieParser from 'cookie-parser'
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
+import { errorMiddleware } from './src/middlewares/errorMiddleware.js'
 
 const app = express()
 dotenv.config({
@@ -14,5 +15,5 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser)
 app.use(cors())
 
-
+app.use(errorMiddleware)
 export default app
